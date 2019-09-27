aa7a47f8cde1a62102dcbdbef0a6040717f7ff69
Max Wilkinson <mawilkin@umich.edu>
2019-09-27 16:34:01 -0400

    Updated site for fall hackathon

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,29 +1,37 @@
 // Inspired by the timer at https://www.w3schools.com/howto/howto_js_countdown.asp
 
-var htmlElement = "timer"
+var htmlElement = 'timer';
 
 // Set the date we're counting down to
-var hackathonStart = new Date("Jun 27, 2019 08:30:00").getTime();
+var hackathonStart = new Date('Nov 7, 2019 08:30:00').getTime();
 
 // Update the count down every 1 second
 var interval = setInterval(function() {
-
   // Find the remaining time between the hackathon start and now
-  var remainingTime = hackathonStart - new Date().getTime();;
+  var remainingTime = hackathonStart - new Date().getTime();
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var hours = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById(htmlElement).innerHTML = days + "d <br/>" + hours + "h <br/>"
-  + minutes + "m <br/>" + seconds + "s <br/>";
+  document.getElementById(htmlElement).innerHTML =
+    days +
+    'd <br/>' +
+    hours +
+    'h <br/>' +
+    minutes +
+    'm <br/>' +
+    seconds +
+    's <br/>';
 
   // When the countdown finishes update the text
   if (remainingTime < 0) {
     clearInterval(interval);
-    document.getElementById(htmlElement).innerHTML = "Now!";
+    document.getElementById(htmlElement).innerHTML = 'Now!';
   }
 }, 1000);
